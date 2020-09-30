34e1d1e99f415876c03a6af865e82d9ade6bf157
Edd <eduardo940531@gmail.com>
2020-09-30 18:19:39 -0400

    Add payment gateway configurations

--- src/@next/components/organisms/CheckoutPayment/fixtures.ts
+++ src/@next/components/organisms/CheckoutPayment/fixtures.ts
@@ -45,6 +45,16 @@ export const paymentGateways: IPaymentGateway[] = [
         value: "false",
       },
     ],
+    id: "mirumee.payments.authorize",
+    name: "Authorize",
+  },
+  {
+    config: [
+      {
+        field: "store_customer_card",
+        value: "false",
+      },
+    ],
     id: "mirumee.payments.dummy",
     name: "Dummy",
   },
